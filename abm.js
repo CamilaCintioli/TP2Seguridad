d69ab4f51970ef2fe34bdde9a1dbb064b90d32ce
camilacintioli <camila.cintioli@gmail.com>
2019-07-06 13:15:24 -0300

    Agrego validacion de API key en la abm

--- abm.js
+++ abm.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (req, res, next) {
+  const apiKey = req.query.apiKey;
+  if (apiKey) {
+    next()
+  } else {
+    res.status(401)
+    res.end()
+  }
+})
+
 app.get('/', function (req, res) {
   console.log('TOP SECRET INFORMATION');
   res.send('TOP SECRET INFORMATION');
